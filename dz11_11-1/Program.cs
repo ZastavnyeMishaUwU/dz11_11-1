76b5b7b1b01ed7a77c029b9dd052c2deadc5b909
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-11-13 20:51:01 +0200

    Add project files.

---
+++ dz11_11-1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace dz11_11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 14, 19, 21, 7, 18 };
+            int target = 7;
+
+            int index = Array.IndexOf(array, target);
+
+            if (index != -1)
+            {
+                Console.WriteLine($"Індекс числа {target}: {index}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {target} не знайдено .");
+            }
+        }
+    }
+}
